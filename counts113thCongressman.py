4027fd56e341232a4ee62a275839fc9e64826a1f
Aravinda Kumar Reddy Yempada <aravind_yempada@outlook.com>
2015-11-20 16:35:38 -0700

    change in counts program

--- counts113thCongressman.py
+++ counts113thCongressman.py
@@ -9,14 +9,15 @@ import pandas as pd
 
 def count(path):
     content = pd.read_csv(path)
-    print 'Total tweets', len(content)
+    print 'Total tweets: ', len(content)
+    print 'Avg per month: ', len(content)/24.0
     
     content.columns = ['name','tweet','time']
     
     byUser = content.groupby('name',sort=False)['tweet'].agg('count').reset_index()[[0,1]]    
     byUser.columns = ['name','tweetscount']    
     
-    byUser.to_csv('totalCount.csv',header=True,index=False)
+    #byUser.to_csv('totalCount.csv',header=True,index=False)
     
     content['month'] = content['time'].apply(lambda x: x.split('-')[1] if x.find('-')!=-1 else '')
     content['year'] = content['time'].apply(lambda x: x.split('-')[0] if x.find('-')!=-1 else '')
@@ -24,8 +25,10 @@ def count(path):
     byMonth = content.groupby(['name','year','month'], sort=False)['tweet'].agg(['count']).reset_index()
     byMonth.columns = ['name','year','month','tweetscount']
     byMonth.to_csv('totalCountbyMonth.csv',header=True,index=False)
-    stats = byMonth.groupby(['name','month'], sort=False)['tweetscount'].agg(['mean','max','min']).reset_index()
-    stats.to_csv('stats.csv',header=True,index=False)
-    print stats
+    #stats = byMonth.groupby(['name','month'], sort=False)['tweetscount'].agg(['mean','max','min']).reset_index()
+    #stats.to_csv('stats.csv',header=True,index=False)
+    print 'Max tweets per month: ', max(byMonth['month'])
+    print 'Min tweets per month: ', min(byMonth['month'])
+    #print stats
     
-count('C:\\Users\\AravindKumarReddy\\Desktop\\Newfolder\\tweets_senators.csv')
+count('C:\\Users\\AravindKumarReddy\\Desktop\\Newfolder\\tweets_reps.csv')
